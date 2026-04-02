21b7019af17a9cc40f7b0a81979a00cf1ae5ea17
Marko-Veljovic <mveljovic2004@gmail.com>
2026-04-02 10:45:01 +0200

    keep only necessary requirements, all python files should be ran as modules

--- scripts/run_visualizations.py
+++ scripts/run_visualizations.py
@@ -1,8 +1,4 @@
 from pathlib import Path
-import sys
-
-PROJECT_ROOT = Path(__file__).resolve().parents[1]
-sys.path.append(str(PROJECT_ROOT))
 
 from src.visualization import (
     generate_group_1_plots,
